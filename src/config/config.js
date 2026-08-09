18ce122861fd3c0c093c10b76356bfa9df7deeba
Debojit Mahapatra <debojitmahapatra19985@gmail.com>
2026-08-10 02:01:15 +0530

    FULL project base line

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const sharedConfig = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || 'teligram_bot',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
